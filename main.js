bc03fffafecc122807580f85748aaa8e83fb33d8
Sebastian <161753458+mdbot345@users.noreply.github.com>
2024-02-29 23:17:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -490,7 +490,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `BOT DOWNLOAD MUSIC [ ⏳ ] Uptime: ${uptime}`;
+  const bio = `BOBIZA BOT [ ⏳ ] Uptime: ${uptime}`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
@@ -501,3 +501,4 @@ function clockString(ms) {
   return [d, 'd ️', h, 'h ', m, 'm ', s, 's '].map((v) => v.toString().padStart(2, 0)).join('');
 }
 _quickTest().catch(console.error);
+          
