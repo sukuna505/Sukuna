ed4913d182cfc2f7cb7e917d05b3bd8f863cb35b
Sebastian <161753458+mdbot345@users.noreply.github.com>
2024-03-01 03:00:00 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -14,15 +14,16 @@ const defaultMenu = {
 *└ Uptime :* %muptime
 %readmore
 > *ميزة تحميل الموسيقى* 
-> اسم الموسيقى او المغني *song.*
+> اسم الأغنية أو المغني *song.*
   `.trimStart(),
 header:'%category',
 body:'%cmd %isdiamond %isPremium',
 footer:'\n',
 after: ` *إستخدامك للبوت بشكل صحيح يعني أنك تزيد من إحتمالية أن يبقى البوت شغالا لمدة أطول . لذا إن واجهتك أي مشكلة لا تخجل من أن تسأل صاحب البوت*
 
-> *Bot Owner Number
-> *+33 6 05 90 72 72*`,
+
+> *Bot Owner Number*
+> ~+33 6 05 90 72 72~`,
 }
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
