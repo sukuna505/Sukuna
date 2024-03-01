89a5597a95eb082c31e9273abbf7372f7cc086e0
Sebastian <161753458+mdbot345@users.noreply.github.com>
2024-03-01 02:07:11 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -19,7 +19,8 @@ const defaultMenu = {
 header:'%category',
 body:'%cmd %isdiamond %isPremium',
 footer:'\n',
-after: '*إستخدامك للبوت بشكل صحيح يعني أنك تزيد من إحتمالية أن يبقى البوت شغالا لمدة أطول . لذا إن واجهتك أي مشكلة لا تخجل من سؤال صاحب البوت
+after: ' *إستخدامك للبوت بشكل صحيح يعني أنك تزيد من إحتمالية أن يبقى البوت شغالا لمدة أطول . لذا إن واجهتك أي مشكلة لا تخجل من أن تسأل صاحب البوت* \n> *رقم صاحب البوت* \n> *+33 6 05 90 72 72*',
+}
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
     let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
