7c7bcda3e8f5afe21771b21f2906f890651951ad
sukuna505 <167900907+sukuna505@users.noreply.github.com>
2024-04-23 19:44:45 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -29,8 +29,7 @@ header:'%category',
 body:'%cmd %isdiamond %isPremium',
 footer:'\n',
 after: `*إستخدامك للبوت بشكل صحيح يعني أنك تزيد من إحتمالية أن يبقى البوت شغالا لمدة أطول*
-
-+33 6 05 90 72 72 `,
+,
 }
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
